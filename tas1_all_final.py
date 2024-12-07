506fc6660cfe5bcad71aea28892609877d045a17
youknow <qp713223@gmail.com>
2024-12-07 19:21:16 +0700

    Update tas1_all_final.py

--- tas1_all_final.py
+++ tas1_all_final.py
@@ -2,8 +2,8 @@ import cv2
 import serial 
 import numpy as np 
 import time
+base_speed = 0.2 #m/s
 #arduino = serial.Serial("INSERT USB",baudrate=9600, timeout= 1)
-max_wheel = 150
 def color_detec(frame):
     hsvcolor = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     lower = np.array([35, 50, 50])  # 7-up bottle's color - suggest from ChatGPT:')
@@ -15,16 +15,25 @@ def color_detec(frame):
         x,y,w,h = cv2.boundingRect(largest_contours)
         return x,y,w,h
     return None
-def calculate_wheel(cx,frame_center):
-    error = cx - frame_center
-    k_p = 0.5
-    v_base = max_wheel // 2 
-    correction = int(k_p * error)
-    v_left = v_base - correction
-    v_right = v_base + correction
-    v_left = max(0, min(max_wheel, v_left))
-    v_right = max(0, min(max_wheel, v_right))
-    return v_left, v_right
+    
+def send_wheel(left_speed,right_speed) 
+command = f"{left_speed} {right_speed}\n"
+arduino.write(command.encode('utf -8')) 
+except serial.SerialException as e: 
+print(f"Error")
+
+def control_robot(frame_center, cx)
+error = cx - frame_center 
+intergral += error 
+previous_error = error 
+derivative = error - previous_error 
+correction = Kp*error + Ki * intergral + Kd * derivative 
+base_speed = 0.2
+left_speed = max(-1.0, min(1.0, base_speed - correction))
+right_speed = max(-1.0, min(1.0, base_speed - correction))
+send_wheel(left_speed,right_speed)
+else:
+send_wheel(0,0)
 
 cam = cv2.VideoCapture(1)
 if not cam.isOpened(): 
@@ -45,13 +54,14 @@ while True:
 
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
         cv2.circle(frame, (cx, cy), 5, (0, 0, 255), -1)
-        print(f"v_left {v_left}, v_right {v_right}")
-        # send data to Arduino
-        # arduino.write(f"{v_left},{v_right}\n".encode())
-
+        #send data to Arduino
+        arduino.write(f"{v_left},{v_right}\n".encode())
+    if bottle is None: 
+        arduino.write("no bottle connect\n".encode())
     cv2.imshow("frame", frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):  # Allows exiting the loop with 'q'
         break
     time.sleep(0.05)
 
 cv2.destroyAllWindows()
+arduino.close()
